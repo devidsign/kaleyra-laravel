fe63d5b8b1ae3ca4b1f6fa592148850dbafebef0
Aldo Personal <zumaran.aldo@gmail.com>
2023-12-18 19:55:43 +0100

    first 2

--- resources/views/components/registration-button.blade.php
+++
@@ -1,7 +0,0 @@
-@props([
-    'room'
-])
-<x-idsign::badge color="positive" class="cursor-pointer flex space-x-2" wire:click.prevent="$dispatchTo('bandyer-registration-modal', 'bandyer-open-registration-modal', { room: {{ $room }} })">
-    <x-idsign-tabler class="h-6 w-6" icon="video"></x-idsign-tabler>
-    <span>Vedi Registrazione</span>
-</x-idsign::badge>
